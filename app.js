228a38c6e3bc4ebc152cb495e59a8a4ee0b7d36e
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-07-25 17:45:20 +0200

    feat: add logger & limiter

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const usersRoute = require('./routes/users');
 const moviesRoute = require('./routes/movies');
 // const auth = require('./middlewares/auth');
 
+const { limiter } = require('./utils/limiter');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { errorsHandler, notFound } = require('./utils/errorsHandler');
+
 mongoose.connect('mongodb://localhost:27017/filmdb');
 
 const { PORT = 3000 } = process.env;
@@ -21,32 +25,31 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(requestLogger);
+app.use(requestLogger);
 
 // app.use(cors({
 //   origin: allowedCors,
 //   credentials: true,
 // }));
 
-// app.use(limiter);
+app.use(limiter);
 
 // app.post('/signin', loginValidation, login);
 // app.post('/signup', registerValidation, createUser);
 
 app.use('/users', usersRoute);
 app.use('/movies', moviesRoute);
-
 // app.use('/users', auth, usersRoute);
 // app.use('/cards', auth, cardsRoute);
 
 // app.post('/signout', auth, logout);
 
-// app.use(notFound);
+app.use(notFound);
 
-// app.use(errorLogger);
+app.use(errorLogger);
 
 // app.use(joiErrors);
 
-// app.use(errorsHandler);
+app.use(errorsHandler);
 
 app.listen(PORT);
