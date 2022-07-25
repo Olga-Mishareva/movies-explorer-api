f00d67d1554d493becbee1197d0f97ba5a2dfd3b
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-07-25 13:05:00 +0200

    feat: add app.js & file structure

---
+++ app.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+// const cors = require('cors');
+
+mongoose.connect('mongodb://localhost:27017/filmdb');
+
+const { PORT = 3000 } = process.env;
+const app = express();
+
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// app.use(requestLogger);
+
+// app.use(cors({
+//   origin: allowedCors,
+//   credentials: true,
+// }));
+
+// app.use(limiter);
+
+// app.post('/signin', loginValidation, login);
+// app.post('/signup', registerValidation, createUser);
+
+// app.use('/users', auth, usersRoute);
+// app.use('/cards', auth, cardsRoute);
+
+// app.post('/signout', auth, logout);
+
+// app.use(notFound);
+
+// app.use(errorLogger);
+
+// app.use(joiErrors);
+
+// app.use(errorsHandler);
+
+app.listen(PORT);
